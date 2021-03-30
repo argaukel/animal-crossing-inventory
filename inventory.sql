9f24d133be7573d4887518ccd54801328e1dc5ef
A Reilly Gaukel <45907561+argaukel@users.noreply.github.com>
2021-03-29 20:30:36 -0500

    table

--- inventory.sql
+++ inventory.sql
@@ -2,3 +2,17 @@ DROP DATABASE IF EXISTS inventory_ACNH;
 CREATE DATABASE inventory_ACNH;
 
 USE inventory_ACNH;
+
+CREATE TABLE trades(
+    id INT NOT NULL AUTO_INCREMENT,
+    item_name VARCHAR(100) NOT NULL,
+    category VARCHAR(45) NOT NULL,
+    invt_quantity INT default 0,
+    sold_quantity INT default 0,
+    bells BOOLEAN NOT NULL,
+    average_bells INT default 0,
+    nmt BOOLEAN NOT NULL,
+    average_nmt INT default 0,
+    sent_taylor BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
